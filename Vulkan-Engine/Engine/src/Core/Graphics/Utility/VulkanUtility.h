3bd218e09a193d75b07bbcd9e3e8b0c775541f86
Ori Lazar <36171247+Kney-Delach@users.noreply.github.com>
2020-01-05 22:07:36 +0000

    removed mailbox presentation, using FIFO so stream doesn't break!

--- Vulkan-Engine/Engine/src/Core/Graphics/Utility/VulkanUtility.h
+++ Vulkan-Engine/Engine/src/Core/Graphics/Utility/VulkanUtility.h
@@ -229,13 +229,14 @@ namespace Vulkan_Engine
 		// 4. MAILBOX (Best->Triple buffering capabilities.... No queue bottleneck)
 		VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes)
 		{
-			for (const auto& availablePresentMode : availablePresentModes) 
-			{
-				if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) 
-				{
-					return availablePresentMode;
-				}
-			}
+			//TODO: Figure out how to sync with vertical blank (without using FIFO)
+			//for (const auto& availablePresentMode : availablePresentModes) 
+			//{
+			//	if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) 
+			//	{
+			//		return availablePresentMode;
+			//	}
+			//}
 			return VK_PRESENT_MODE_FIFO_KHR;
 		}
 
